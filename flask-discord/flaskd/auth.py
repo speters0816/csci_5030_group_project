884fa8fb444eba5e60b9598cc03f125cda1d1820
Sam Peters <samuel.peters@slu.edu>
2022-03-02 07:39:48 -0600

    Add function to load logged in user data from user table to session.

--- flask-discord/flaskd/auth.py
+++ flask-discord/flaskd/auth.py
@@ -11,4 +11,14 @@ from flaskr.db import get_db
 bp = Blueprint('auth',__name__,url_prefix='/auth')
 
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+                'SELECT * FROM user WHERE id = ?', (user_id,)
+                ).fetchone() # Grabs all columns in the user table where user_id is found. Load this in the session
 
