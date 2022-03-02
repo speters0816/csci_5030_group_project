51d8a997292f20b7b37cef94b5e8108c6eefaf5c
Sam Peters <samuel.peters@slu.edu>
2022-03-02 07:56:35 -0600

    Add login required function that will be used for index html and all user specific html channels

--- flask-discord/flaskd/auth.py
+++ flask-discord/flaskd/auth.py
@@ -1,12 +1,12 @@
 import functools
 
-from Flask import ( 
+from flask import ( 
         Blueprint, flash, g, redirect, render_template, request, session, url_for
         )
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from flaskd.db import get_db
 
 bp = Blueprint('auth',__name__,url_prefix='/auth')
 
@@ -22,3 +22,12 @@ def load_logged_in_user():
                 'SELECT * FROM user WHERE id = ?', (user_id,)
                 ).fetchone() # Grabs all columns in the user table where user_id is found. Load this in the session
 
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('login_1'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
