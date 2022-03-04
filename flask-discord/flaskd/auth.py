0e5fe3dac744836f62593b98620aa0631851c238
Sam Peters <samuel.peters@slu.edu>
2022-03-04 07:27:26 -0600

    Change long route to /auth/login

--- flask-discord/flaskd/auth.py
+++ flask-discord/flaskd/auth.py
@@ -51,7 +51,7 @@ def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
         
         return view(**kwargs)
     
