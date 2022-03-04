7e7b734e36df71f8e2d2b2d0cdab1bd871b34647
Sam Peters <samuel.peters@slu.edu>
2022-03-04 07:14:01 -0600

    Added login function to auth.py blueprint

--- flask-discord/flaskd/auth.py
+++ flask-discord/flaskd/auth.py
@@ -10,6 +10,31 @@ from flaskd.db import get_db
 
 bp = Blueprint('auth',__name__,url_prefix='/auth')
 
+@bp.route("/login",methods=("GET","POST"))
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user.execute("SELECT * FROM user WHERE email = ?", (email,)
+                ).fetchone()
+        
+        pass_check = check_password_hash(user["password"],password)
+
+        if user == None or pass_check == None:
+            error = "Incorrect Email and Password Combination!"
+        
+        if error == None:
+            session.clear()
+            session["user_id"] = user["id"]
+
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("login_1.html")
+
 
 @bp.before_app_request
 def load_logged_in_user():
